49f1e7ee8a5f10629a1148c0a489fdcf338f20c9
albert carrasquel <albertasbel@gmail.com>
2026-03-28 17:51:38 -0300

    first commit

---
+++ env.js
@@ -0,0 +1,15 @@
+// ==========================================
+// VARIABLES DE ENTORNO LOCALES
+// ==========================================
+// IMPORTANTE: Asegúrate de añadir 'env.js' a tu archivo .gitignore 
+// para no exponer estas variables en repositorios públicos.
+
+window.env = {
+    // API Keys de Firebase (Técnicamente son públicas, pero es buena práctica no subirlas)
+    FIREBASE_API_KEY: "AIzaSyDIt41mWTf6DGYGRS0nr9zf4aSpyJQyNRc",
+    FIREBASE_PROJECT_ID: "gym-tracker-94c3f",
+    FIREBASE_APP_ID: "1:193142913607:web:09b5eb0c6f6d0254380f62",
+    
+    // ID Temporal inyectado para pruebas evadiendo reglas de seguridad
+    TEST_USER_ID: "bMHBCTVIm6SL7w0KE76OOJn0Njh1" 
+};
